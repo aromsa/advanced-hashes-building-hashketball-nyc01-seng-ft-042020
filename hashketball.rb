5ef5b0a754a5aa31b8cfc359285946a9c91be299
anna bateman <annacbateman@gmail.com>
2020-04-16 16:17:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -61,12 +61,17 @@ def game_hash
       :team_name => "Charlotte Hornets", 
       :colors => ["Turquoise", "Purple"], 
       :players => [{
+        :player_name => "Jason Terry", 
+        :number => 31, 
+        :shoe => 15, 
+        :points => 19, 
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1}
       
-        
-        
-        
-        
-      }]}
+      ]}
   }
 end
   
