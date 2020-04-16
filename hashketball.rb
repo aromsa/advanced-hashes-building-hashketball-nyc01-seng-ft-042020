365b4b8c951aea56511cb6eaed863502abb8737e
anna bateman <annacbateman@gmail.com>
2020-04-16 15:56:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,8 +7,8 @@ def game_hash
       :colors => ["Black", "White"], 
       :players => [{
         :player_name => "Alan Anderson", 
-        :number => , 
-        :shoe => , 
+        :number => 0, 
+        :shoe => 16, 
         :points => , 
         :rebounds => ,
         :assists => ,
