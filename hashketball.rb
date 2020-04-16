fd1920fafacb58e7cbc20caee819b4a9ea35cbc8
anna bateman <annacbateman@gmail.com>
2020-04-16 15:45:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     :home => {
       :team_name => "", 
       :colors => [], 
-      :players => [{:player_name => }{:number => }{:shoe}]}, 
+      :players => [{:player_name => }{:number => }{:shoe}{}]}, 
     :away => {
       :team_name => "", 
       :colors => [], 
